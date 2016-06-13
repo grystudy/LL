d311714180c5ca8b7495dcc009444ad64e7d0281
yangy <yangy@meixing.com>
2016-06-13 11:21:56 +0800

    上传文件调查ok

--- llInputWeb/app/controllers/home_controller.rb
+++ llInputWeb/app/controllers/home_controller.rb
@@ -1,4 +1,37 @@
 class HomeController < ApplicationController
   def index
   end
+  
+  def resetByFile
+  end  
+  
+  def resetByFileComplete
+    unless request.get?
+      if filename=uploadFile(params[:file]['file'])
+        render :text=>filename
+      end
+    end
+  end
+  
+  def uploadFile(file)
+   if !file.original_filename.empty?
+     filename=getFileName(file.original_filename)
+
+     dirName="#{Rails.root}/public/input" 
+     if(!File.directory?(dirName))
+      Dir.mkdir(dirName)
+    end
+
+    File.open(File.join(dirName,filename), "wb") do |f|
+     f.write(file.read)
+   end
+   return filename
+ end
+end
+
+def getFileName(filename)
+	if !filename.nil?
+   return filename
+ end
+end
 end
--- llInputWeb/config/routes.rb
+++ llInputWeb/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'home/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-get 'home/index'
+get 'home/resetByFile'
+post 'home/resetByFileComplete'
 root 'home#index'
 end
