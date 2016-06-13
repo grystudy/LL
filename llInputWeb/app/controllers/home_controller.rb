d1a8ff86a70ddbab4262c83908b143bc6918aa4a
yangy <yangy@meixing.com>
2016-06-13 17:27:39 +0800

    显示限行主界面

--- llInputWeb/app/controllers/home_controller.rb
+++ llInputWeb/app/controllers/home_controller.rb
@@ -1,37 +1,104 @@
 class HomeController < ApplicationController
   def index
   end
+
+  $main_data = []
+  def main
+  end
   
   def resetByFile
   end  
   
   def resetByFileComplete
     unless request.get?
-      if filename=uploadFile(params[:file]['file'])
-        render :text=>filename
+      file_input = params[:fileMain]
+      return if !file_input
+      if uploadFile(file_input)
+        render  layout: "application" ,inline: "<p>#{file_input.original_filename} 上传成功</p>"
+      else
+        render  layout: "application" ,inline: "<p>#{file_input.original_filename} 上传失败</p>"
       end
     end
   end
   
+  private  
+
+  def convertUI(data)
+    return nil if !data
+    result = []
+    data.each_with_index do |line,i|
+      return nil if !line || line.length!=16
+      result << convertLine(line)
+    end
+
+    result
+  end
+
+  def convertLine(line)
+    line
+  end
+
   def uploadFile(file)
    if !file.original_filename.empty?
-     filename=getFileName(file.original_filename)
 
-     dirName="#{Rails.root}/public/input" 
-     if(!File.directory?(dirName))
+    dirName="#{Rails.root}/public/input" 
+
+    if(!File.directory?(dirName))
       Dir.mkdir(dirName)
     end
 
-    File.open(File.join(dirName,filename), "wb") do |f|
-     f.write(file.read)
-   end
-   return filename
+    begin
+      temp_path = File.join(dirName,request.remote_ip()+"_inputMainDataTemp.txt")
+      File.open(temp_path, "wb") do |f|
+        f.write(file.read)
+      end
+      data = Read(temp_path)
+      data_for_view = convertUI(data)
+      return nil if !data_for_view
+      $main_data = data_for_view
+    rescue 
+      return nil
+    end
+
+    return nil if !data
+    Write(File.join(dirName,"inputMainData.txt"),data)
+
+    return data
+  else
+   return nil
  end
 end
 
-def getFileName(filename)
-	if !filename.nil?
-   return filename
- end
+Tab="\t"
+New_Line="\n"
+
+def Read(fileName)
+  File.open(fileName,"r", :encoding => 'UTF-8') do |io|
+    lines=[]
+    io.each do |line|
+      array = line.chomp.split(Tab)                            
+      lines << array
+    end
+    lines
+  end
 end
+
+def Write(fileName, data)
+  return if !data
+
+  dirName=File.dirname(fileName)      
+  if(!File.directory?(dirName))
+    Dir.mkdir(File.dirname(fileName))
+  end
+
+  File.open(fileName, "w", :encoding => 'UTF-8') do |io|
+    data.each_with_index do |line,i|
+      io.write line.join(Tab)
+      if i != data.count - 1            
+        io.write(New_Line)
+      end
+    end
+  end
+end 
+
 end
--- llInputWeb/config/routes.rb
+++ llInputWeb/config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 get 'home/resetByFile'
 post 'home/resetByFileComplete'
+get 'home/main'
+match 'home/resetByFileComplete' => 'home#index' , :via => :get
+
 root 'home#index'
 end
