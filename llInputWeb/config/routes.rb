02eb758a34b065afaa169fc1d4ce30448065700f
yangy <yangy@meixing.com>
2016-06-17 11:29:36 +0800

    根据nigix要求修改项目配置

--- llInputWeb/config/routes.rb
+++ llInputWeb/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
- # get 'home/index'
+scope(:path => '/llweb') do 
+# get 'home/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 get 'home/resetByFile'
@@ -12,4 +13,5 @@ match 'home/edit_item' => 'home#main' , :via => :get
 match 'home/index' => 'home#main' , :via => :get
 get 'home/download'
 root 'home#index'
+end
 end
