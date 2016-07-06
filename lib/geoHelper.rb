c4dfac5e6cfe3daa53fe0df05b7deec9fe89cbc7
yangy <yangy@meixing.com>
2016-07-06 11:53:39 +0800

    求交点方法实现

--- lib/geoHelper.rb
+++ lib/geoHelper.rb
@@ -8,7 +8,38 @@ module GeoHelper
 			regular_direction exterior_ring_
 			regular_direction interior_ring_
 
+			cross_res = get_cross_points(exterior_ring_, interior_ring_)
+		end
+
+		def get_cross_points(exterior_ring_, interior_ring_)
 			inner_box = calc_boundingbox interior_ring_
+			# raise 'nobox' unless inner_box
+			return nil unless inner_box
+			try_get_cross = ->(v_){
+				ex_res = calc_cross_point(v_, exterior_ring_)
+				in_res = calc_cross_point(v_, interior_ring_)
+				return nil unless ex_res && in_res
+				[ex_res,in_res]
+			}
+			stride = inner_box.x_span / 30.0
+			cur_v = inner_box.center_x
+
+			excute = Proc.new { 
+				puts cur_v
+				res_temp = try_get_cross.call(cur_v)
+				return res_temp if res_temp
+			}
+			while cur_v < inner_box.max_x
+				excute.call
+				cur_v = cur_v + stride
+			end
+
+			cur_v = inner_box.center_x
+			while cur_v > inner_box.min_x
+				excute.call
+				cur_v = cur_v - stride
+			end
+			nil
 		end
 
 		def calc_cross_point (v_ , ring_)
@@ -18,16 +49,12 @@ module GeoHelper
 				p_b_ = ring_[i+1]
 				res = linear_interpolation(v_,p_a_,p_b_)
 				if res
-					if res[:b]
-						points<< p_a_
-					else
-						p = res[:p]
-						points<< p if p
-					end
+					res[:i] = i
+					points << res
 				end
 			end
 
-			points.length>0 ? points : nil
+			points.length == 2 ? points : nil
 		end
 
 		def is_ring? points_
@@ -57,17 +84,24 @@ module GeoHelper
 			ratio = (a_diff/dis_x).abs
 			{p:@factory.point(v_,p_a_.y-ratio*dis_y)}
 		end
+
+		attr_reader :factory
 	end
 end
 
-@factory = ::RGeo::Cartesian.simple_factory
-p1 = @factory.point(1, 1)
-p2 = @factory.point(2, 4)
-p3 = @factory.point(5, 2)
+# test
+factory = GeoHelper.factory
+p1 = factory.point(1, 1)
+p2 = factory.point(2, 4)
+p3 = factory.point(5, 2)
 array = [p1,p2,p3,p1].reverse
-# puts GeoHelper.convert_polygon_with_hole(array,array).center_x
+puts GeoHelper.convert_polygon_with_hole(array,array)
 
 puts GeoHelper.calc_cross_point(1.5,array)
+
+point5 = factory.point(0, 1)
+point6 = factory.point(0, 2)
+point7 = factory.point(-1, 1)
 
 # p GeoHelper.linear_interpolation(1.5,p1,p2)
 
